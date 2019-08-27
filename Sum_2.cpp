0ec4c35070c1ab89ca553cd6c328e27d947c6865
Rohan Samanta <42403165+espatatis@users.noreply.github.com>
2019-08-28 03:51:50 +0530

    Create Sum_2.cpp

---
+++ Sum_2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdio.h>
+
+int main(){
+
+float x, y, z;
+ cout<<"Enter value 1 : \n";
+ cin>>x;
+ cout<<"Enter value 2 : \n";
+ cin>>y;
+ z = x + y ;
+ cout<<"The sum of "<<x<<"and "<<y<<"is : "<<z;
+ return 0;
+ 
+}
